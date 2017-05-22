32c38d98527872aecc482c487cc7e69f60276e92
Nguyenvo <nguyenvo1714@gmail.com>
2017-05-22 22:23:29 +0700

    PR_CAR-Submenu creating submenu

--- resources/views/sites/transfers/viewTransfers.blade.php
+++ resources/views/sites/transfers/viewTransfers.blade.php
@@ -1,46 +1,6 @@
-@extends('sites.layout.app')
+@extends('sites.layout.submenu')
 @section('content')
 	<div class="wrapcontent">
-		<section class="search-container">
-			<div class="cttopdeal">
-				<div class="img-bgtour" style="background-image:url('https://goasiadaytrip.com/img/halong-bay-sunset.jpg');"></div>
-				<div class="overlay-darkquare overlay-darken"></div>
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-12 col-md-10 col-md-offset-1">
-							{!! Form::open(['url' => '/find-transfer', 'method' => 'POST', 'class' => 'form-inline search-form col-md-12', 'id' => 'searchForm']) !!}
-								<div class="form-group col-md-4 col-xs-12">
-								<label class="control-label" for="pick-up">
-									Pick-up
-								</label>
-								<div class="wrapper-input">
-									<input id="pick-up" class="form-control col-md-7 col-xs-12 pick-up input-text" name="pick-up" placeholder="Type airport, city or train station" required="required" type="text">
-									</div>
-								</div>
-								<!-- <div class="form-group col-md-1 col-xs-12">
-									<a href="javascript:void(0)" class="swap-locations">
-										<i class="fa fa-exchange"></i>
-									</a>
-					            </div> -->
-								<div class="form-group col-md-4 col-xs-12">
-									<label class="control-label" for="drop-off">
-										Drop-off
-									</label>
-									<div class="wrapper-input">
-										<input id="drop-off" class="form-control col-md-7 col-xs-12 drop-off input-text" name="drop-off" placeholder="Type airport, city or train station" required="required" type="text">
-									</div>
-								</div>
-								<div class="form-group col-md-4 col-xs-12">
-									<button id="send" type="submit" class="button button-red">
-										<span class="glyphicon glyphicon-search"></span>Find transfer
-									</button>
-								</div>
-							{!! Form::close() !!}
-						</div> <!-- End Wrap Quicksearch -->
-					</div>
-				</div>
-			</div>
-		</section>
 		<section class="container-fluid">
 			<!-- <div class="container"> -->
 				<div class="row">

d4bc62e72802bbd2a2f039d942aeda450740f993
Vo Cong Nguyen <vcnguyen@tma.com.vn>
2017-05-24 18:15:10 +0700

    PR_CAR-Authenadmin created authen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         app('view')->composer('admin.layout.app', function($view) {
             $action =  app('request')->route()->getAction();
 

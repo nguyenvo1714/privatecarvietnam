3cd49d18a1d72e897d8e37f0eff15cedeb2a6640
Nguyenvo <nguyenvo1714@gmail.com>
2017-05-28 21:45:11 +0700

    staging replace parallax

--- resources/views/sites/layout/app.blade.php
+++ resources/views/sites/layout/app.blade.php
@@ -39,7 +39,7 @@
         {!! Html::script('js/jquery-ui.min.js') !!}
         {!! Html::script('js/jquery.validate.min.js') !!}
         {!! Html::script('js/additional-methods.min.js') !!}
-        {!! Html::script('js/parallax.min.js') !!}
+        <!-- {!! Html::script('js/parallax.min.js') !!} -->
         {!! Html::script('js/wow.min.js') !!}
         {!! Html::script('js/owl.carousel.min.js') !!}
         {!! Html::script('js/jquery.validate.min.js') !!}
